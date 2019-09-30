983e3f2df77a22c60bff50b3d906d1159028c3ca
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2019-09-30 21:38:15 +0200

    Detail and display

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,11 +4,14 @@ import queryString from "query-string";
 
 //location.search = localhost:8080?moveId=200
 const parsed = queryString.parse(location.search);
-// queryString.parse("?moveId=200"); => {movieId: 200}
+// queryString.parse("?movieId=200"); => {movieId: 200}
 // queryString.parse(""); => {}
 
 if(parsed.movieId){
-    //detail
-} else {
-    //overzicht
+    const overzicht= document.querySelector("#overzicht");
+    overzicht.style.display= "none";
+} 
+else {
+    const detail= document.querySelector("#detail");
+    detail.style.display= "none";
 }
