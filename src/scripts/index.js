736a2e763b17323b1e00e2de5531fabff54885de
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2019-09-30 21:01:23 +0200

    Pushing the files

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+import '../styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const number= Math.random();
+if(number>0.5){
+    const naam= "Carl";
+    console.log(naam);
+}
+else{
+    const naam= "Karel";
+    console.log(naam);
+}
