99fed059772acc4bed87b1aa54e701464ff571de
Yves Loeys <>
2019-09-30 21:41:15 +0200

    testcommit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
-import '../styles/index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import queryString from "query-string";
 
 //location.search = localhost:8080?moveId=200
@@ -7,8 +7,8 @@ const parsed = queryString.parse(location.search);
 // queryString.parse("?moveId=200"); => {movieId: 200}
 // queryString.parse(""); => {}
 
-if(parsed.movieId){
-    //detail
+if (parsed.movieId) {
+  //detail
 } else {
-    //overzicht
+  //overzicht
 }
