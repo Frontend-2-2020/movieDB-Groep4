f4228a8cedd5e80ee9a80857db9b7e302bc31b34
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2019-10-02 20:18:15 +0200

    axios call

--- src/scripts/overzicht.js
+++ src/scripts/overzicht.js
@@ -1,4 +1,13 @@
+import Axios from "axios";
+
+
+
 export function initOverzicht(){
     const detail= document.querySelector("#detail");
     detail.style.display= "none";
+    
+    Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5a01120a4a3594cd3fa817cfcb63a4e0")
+    .then(function(response){
+        console.log(response.data);
+    })
 }
