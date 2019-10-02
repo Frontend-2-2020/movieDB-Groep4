c73df70e4fc38bc4b0e3b1900011a061f1520c12
Yves Loeys <>
2019-10-02 20:00:32 +0200

    qadd axios

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -1,4 +1,12 @@
-export function initDetail(){
-    const overzicht= document.querySelector("#overzicht");
-    overzicht.style.display= "none";
+export function initDetail() {
+  const overzicht = document.querySelector("#overzicht");
+  overzicht.style.display = "none";
 }
+
+const axios = require("axios");
+
+// Make a request for a user with a given ID
+axios.get("https://api.themoviedb.org/3/movie/429617").then(function(response) {
+  // handle success
+  console.log(response);
+});
