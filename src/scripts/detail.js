02950b72769560af00baf53f6a23bdead34ed830
Yves Loeys <>
2019-10-06 13:32:30 +0200

    made some changes in the detail page,also changed the cards in the overview so they have equal size now ,placed the description button in the middle and gave it a fixed margin-bottom. also changed the navbar text to the logo

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-
+// when a movieID is parsed ,do not display 'overzicht'
 export function initDetail(id) {
   const overzicht = document.querySelector("#overzicht");
   overzicht.style.display = "none";
@@ -10,8 +10,9 @@ export function initDetail(id) {
       id +
       "?api_key=dc37b97357fd291174e896aa7ce44636"
   ).then(function(response) {
+    //remove console.log when done!
     console.log(response.data);
-
+    //fetching the required data and placeit into the HTML using .innerHTML
     document.getElementById("backdrop").style.backgroundImage =
       "url('https://image.tmdb.org/t/p/w1280" +
       response.data.backdrop_path +
@@ -25,14 +26,14 @@ export function initDetail(id) {
 
     const production = response.data.production_companies;
     let prodHtml = "";
-
+    //looping through the production houses array
     for (let index = 0; index < production.length; index++) {
       const company = production[index];
       prodHtml = prodHtml + " -- " + company.name;
     }
 
     document.getElementById("production").innerHTML = prodHtml;
-
+    //fetching the image, give it a proper size , in this case W500, url+size+path
     document.getElementById("poster").style.backgroundImage =
       "url('https://image.tmdb.org/t/p/w500" + response.data.poster_path + "')";
   });
--- src/scripts/overzicht.js
+++ src/scripts/overzicht.js
@@ -1,23 +1,27 @@
 import Axios from "axios";
 
-export function initOverzicht(){
-    const detail= document.querySelector("#detail");
-    detail.style.display= "none";
-    
-    Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5a01120a4a3594cd3fa817cfcb63a4e0")
-    .then(function(response){
-        var overzichtMovieData=response.data.results;
-        console.log(overzichtMovieData);
-        var allBlock="";
-        for (let index = 0; index < overzichtMovieData.length; index++) {
-            const overzichtElement = overzichtMovieData[index];
-            console.log(overzichtElement);
-            var title= overzichtElement.title;
-            var release= overzichtElement.release_date;
-            var score= overzichtElement.vote_average;
-            var poster=  "https://image.tmdb.org/t/p/w500/" +overzichtElement.poster_path;
-            var movieId= "index.html?movieId="+ overzichtElement.id;
-            var block= `
+export function initOverzicht() {
+  const detail = document.querySelector("#detail");
+  detail.style.display = "none";
+  // fetching the data through Axios
+  Axios.get(
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5a01120a4a3594cd3fa817cfcb63a4e0"
+  ).then(function(response) {
+    var overzichtMovieData = response.data.results;
+    console.log(overzichtMovieData);
+    var allBlock = "";
+    for (let index = 0; index < overzichtMovieData.length; index++) {
+      const overzichtElement = overzichtMovieData[index];
+      // remove console.log when done !
+      console.log(overzichtElement);
+      var title = overzichtElement.title;
+      var release = overzichtElement.release_date;
+      var score = overzichtElement.vote_average;
+      var poster =
+        "https://image.tmdb.org/t/p/w500/" + overzichtElement.poster_path;
+      var movieId = "index.html?movieId=" + overzichtElement.id;
+      // pasting the HTML in JS using the backticks en placing it into a variable
+      var block = `
             <div class="col-lg-3 col-md-4 col-sm-6 col-12">
             <div class="card mb-4">
               <img
@@ -29,14 +33,15 @@ export function initOverzicht(){
                 <h5 class="card-title">${title}</h5>
                 <p class="card-text">Released: ${release}</p>
                 <p class="card-text">Score: ${score}</p>
+                <div class="col-xs-4 text-center">
                 <a href="${movieId}" class="btn btn-primary">Description</a>
+                </div>
               </div>
             </div>
-          </div>`
-         allBlock+= block;
-        } 
-        var moviePage= document.querySelector("#moviePage");
-        moviePage.innerHTML= allBlock;
-    })
-    
+          </div>`;
+      allBlock += block;
+    }
+    var moviePage = document.querySelector("#moviePage");
+    moviePage.innerHTML = allBlock;
+  });
 }
